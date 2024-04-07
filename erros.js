0c8bc59c451b4491ce9d144a69130b2bc3ff0d20
GABRIELMENEDINPEREIRA <miguelmenedinpereira01@gmail.com>
2024-04-07 13:26:45 -0300

    aula-back-end

---
+++ erros.js
@@ -0,0 +1,2 @@
+//console.log(varialvel);
+console.log('oi');
---
+++ tipo-string.js
@@ -0,0 +1,14 @@
+const estudante = "Caroline";
+const doncente = 'Ana';
+const cumprimento = "nosso lema é 'estudar bastante!' ";
+const citacao = `gab diz: "nosso lema é 'estudar bastante!' "`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`a estudante chama ${estudante}`);
+
+// template stirng
+
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
